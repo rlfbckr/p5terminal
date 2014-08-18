bb2862015e03aaf8e2b33d0b9be2b521a57e1fa2
Ralf Baecker <rlfbckr@gmail.com>
2014-08-18 10:55:42 +0200

    gpl

--- src/processing-terminal.h
+++ src/processing-terminal.h
@@ -1,3 +1,21 @@
+/*
+    Copyright (c) 2014 by Ralf Baecker <rlfbckr ~AT~ gmail.com>
+
+    This file is part of Processing-Terminal.
+
+    A wrapper around libcaca and ncurses to offer an ascii-art version to
+    the processing language
+
+    Processing-Terminal is distributed in the hope that it will be
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Processing-Terminal.  If not, see
+    <http://www.gnu.org/licenses/>. 
+*/
+
 #ifndef PROCESSING_TERMINAL_H
 #define PROCESSING_TERMINAL_H
 #include <stdio.h>
